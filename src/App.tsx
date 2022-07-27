a909756f6519947fd8a80625f1720f356a838ae8
Omar-Hosny1 <omarhosny810@yahoo.com>
2022-07-27 23:11:42 +0200

    Todo Item Component Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 function App() {
-  const todos = [new Todo("learn react"), new Todo("learn TS")];
+  const todos = [
+    new Todo("learn react"),
+    new Todo("learn TS"),
+    new Todo("learn TSA"),
+  ];
   return (
     <div className="App">
       <Todos items={todos} />
